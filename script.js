5810629c7be30b4328af54e0f94112382fa44f4f
Ramakrishnan R <ramki.r@indiyaa.com>
2021-12-27 23:49:52 +0530

    Final Done

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ function compute()
     if(principal <=0){
     alert ("Enter a positive number");
     document.getElementById("principal").focus();
-    }
-
-    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML=""
+    } else {
+        var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        }
+
 }
+
+    
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
